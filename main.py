7898c36f300ebd8a80f215eaef6f7b27f0d687ad
Iakl <estebaniakl@gmail.com>
2023-05-15 07:41:18 +0000

    Updated grid

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
 from PIL import Image, ImageDraw
 import series
 
+class ImageNFT:
 
-def draw_serie(image, serie, name):
+  def __init__(self, w, h, margin, bgcolor="blue"):
+      # Create a new image with RGB mode 
+    self.margin = margin
+    self.image = Image.new("RGB", (w, h), "white")
+    draw = ImageDraw.Draw(self.image)
+    draw.rectangle((margin, margin, w - margin, h - margin), fill=bgcolor)
+  
+  
+  def draw_grid(self, num_rows, num_cols, lcolor="black"):
+      # get the width and height in pixels of the image img
+    width, height = self.image.size
+    cell_w = int((width -2*self.margin) / num_rows)
+    cell_h = int((height -2*self.margin) / num_cols)
+
+    draw = ImageDraw.Draw(self.image)
+
+    for x in range(self.margin, width - self.margin, cell_w):
+        draw.line([(x, self.margin), (x, height - self.margin)], fill=lcolor)
+
+    for y in range(self.margin, height - self.margin, cell_h):
+        draw.line([(self.margin, y), (width - self.margin, y)], fill='black')
+
+  def draw_serie(self, serie, name):
     # Save the image as a JPG
     image.save(f"{name}.jpg", "JPEG")
 
-
-def new_base_image(w, h, margin):
-    # Create a new image with RGB mode
-    image = Image.new("RGB", (w, h), "white")
-    draw = ImageDraw.Draw(image)
-    draw.rectangle((margin, margin, w - margin, h - margin), fill="blue")
-    return image
-
-
-def draw_grid(image, num_rows, num_cols):
-    # get the width and height in pixels of the image img
-    width, height = image.size
-    cell_w = int(width / num_rows)
-    cell_h = int(height / num_cols)
-
-    draw = ImageDraw.Draw(image)
-
-    for x in range(0, width, cell_w):
-        draw.line([(x, 0), (x, height)], fill='black')
-
-    for y in range(0, height, cell_h):
-        draw.line([(0, y), (width, y)], fill='black')
-
-    # return image
+  def savejpg(self, name):
+    self.image.save(f"{name}.jpg", "JPEG")
 
 size = 300
 margin = 10
 cells = 10
 
-image = new_base_image(size, size, margin)
-draw_grid(image, cells, cells)
+image = ImageNFT(size, size, margin)
+image.draw_grid(cells, cells)
 
-image.save('grid.png')
+image.savejpg('grid.png')
 
 # Prime series:
 # x = 10
